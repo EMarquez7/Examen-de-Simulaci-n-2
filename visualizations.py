56f32e007ce3bdc12b6c613f9617ed831540574d
EstebanMqz <emarquez1895@gmail.com>
2023-05-11 09:32:02 -0600

    Resampling plots and stats

--- visualizations.py
+++ visualizations.py
@@ -72,9 +72,66 @@ def selection_data(data, rf, title):
     bars = summary.plot.bar(figsize=(20, 10), rot=90, title=title, fontsize=15, grid=True, edgecolor="black", linewidth=1)
     plt.grid(color='gray', linestyle='--')
 
-    summary = d.Markdown(tabulate(summary, headers = "keys", tablefmt = "pipe"))
+    markdown = d.Markdown(tabulate(summary, headers = "keys", tablefmt = "pipe"))
 
-    return display(summary), bars
+    return display(markdown), bars, summary
+
+
+def Mo_stats(dataframe, Selection, P,  title, start, end, percentiles, color):
+    """
+    Mo_stats is a function that makes boxplots of a Monthly resample over a Selection within a dataframe (Q1, mean, Q3, min, max).
+    It also returns a dataframe of statistics containing No° of resamples, mean, std, min, max, percentiles, skewness and kurtosis of resampled data.
+    Parameters:
+    ----------
+    dataframe : dataframe
+        Dataframe from which a Selection is performed.
+    Selection : list
+        Selection to resample on a W, M, Q or Y basis whose period is longer than dataframe.
+    P : str
+        Period of resample (e.g. "W" for Weekly, "M" for monthly, "Q" for quarterly, "Y" for yearly).
+    title : str
+        Title of the boxplot
+    start : str
+        Start date of the dataframe for the boxplot title.
+    end : str
+        End date of the dataframe for the boxplot title.
+    percentiles : list
+        List of percentiles to return in statistics dataframe (e.g. [.05, .25, .5, .75, .95]).
+    color : str
+        Color of the boxplot.
+    Returns:
+    -------
+    describe : dataframe
+        Summary statistics of data selection with sharpe/sortino ratios with given data with specified resample period and percentiles.
+    """
+    
+    Selection = (dataframe[Selection.index].pct_change()).iloc[1:, :].dropna(axis = 1)
+    Selection.index = pd.to_datetime(Selection.index)
+    Selection_Mo_r = Selection.resample(P).sum()
+
+
+    Selection_Mo_r.plot(kind = "box", figsize = (30, 18),
+                      title = title + str(start) + " to " + str(end), color = color, fontsize = 13)
+    
+    for i in range(0, len(Selection_Mo_r.columns)):
+        plt.text(x = i+1 , y = Selection_Mo_r.iloc[:, i].mean(), s = str(round(Selection_Mo_r.iloc[:, i].mean(), 4)), fontsize = 8, color = "lightyellow")
+        plt.text(x = i+1 , y = Selection_Mo_r.iloc[:, i].max() +.02, s = str(round(Selection_Mo_r.iloc[:, i].max(), 3)), fontsize = 7.5, color = "lightgreen")
+        plt.text(x = i+1 , y = Selection_Mo_r.iloc[:, i].min() -.02, s = str(round(Selection_Mo_r.iloc[:, i].min(), 3)), fontsize = 7.5, color = "orange")
+
+    describe = Selection_Mo_r.describe(percentiles).T
+    describe["skewness"] = st.skew(Selection_Mo_r)
+    describe["kurtosis"] = st.kurtosis(Selection_Mo_r)
+    
+    plt.title(title + str(start) + " to " + str(end), fontsize = 14)
+    plt.axhline(0, color = "red", lw = .5, linestyle = "--")
+    plt.xticks(rotation = 90)
+    plt.yticks(np.arange(round(Selection_Mo_r.min().min(), 1), round(Selection_Mo_r.max().max(), 1), 0.05))
+    plt.grid(alpha = 0.5, linestyle = "--", color = "grey")
+    plt.show()
+    
+    return describe
+
+
 
 
 def Optimizer(SP, rf, title):
